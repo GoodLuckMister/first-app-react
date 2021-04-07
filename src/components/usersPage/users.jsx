258b4af9ef1c8ca7084a583930276a67cc35d727
GodLuckMister <paet14@gmail.com>
2021-04-07 16:50:33 +0300

    create new get request API on samurai and add this

--- src/components/usersPage/users.jsx
+++ src/components/usersPage/users.jsx
@@ -1,45 +1,24 @@
+import * as axios from "axios";
 import React from "react";
+import userPhoto from "../../images/image.jpg";
+console.log(userPhoto);
 
 const Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        imageLink:
-          "https://images11.popmeh.ru/upload/img_cache/276/276226951557a7e898a39994ebf744d9_ce_810x810x561x0_cropped_800x800.jpeg",
-        followed: true,
-        name: "Andre",
-        statusPage: "description",
-        cityName: "Kiev",
-        countryName: "Ukraine",
-      },
-      {
-        id: 2,
-        imageLink:
-          "https://images11.popmeh.ru/upload/img_cache/276/276226951557a7e898a39994ebf744d9_ce_810x810x561x0_cropped_800x800.jpeg",
-        followed: true,
-        name: "Dmitri",
-        statusPage: "description",
-        cityName: "Moscow",
-        countryName: "Russia",
-      },
-      {
-        id: 3,
-        imageLink:
-          "https://images11.popmeh.ru/upload/img_cache/276/276226951557a7e898a39994ebf744d9_ce_810x810x561x0_cropped_800x800.jpeg",
-        followed: false,
-        name: "Ignat",
-        statusPage: "description",
-        cityName: "Minsk",
-        countryName: "Belarus",
-      },
-    ]);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
   }
   return (
     <div>
       {props.users.map((u) => (
         <div key={u.id}>
-          <img src={u.imageLink} width={150} />
+          <img
+            src={u.photos.small != null ? u.photos.small : userPhoto}
+            width={150}
+          />
           <div>
             {u.followed ? (
               <button onClick={() => props.unfollow(u.id)}>unfollow</button>
@@ -49,9 +28,9 @@ const Users = (props) => {
           </div>
           <div>
             <div>{u.name}</div>
-            <div>{u.statusPage}</div>
-            <div>{u.cityName}</div>
-            <div>{u.countryName}</div>
+            <div>{u.status}</div>
+            <div>{"u.cityName"}</div>
+            <div>{"u.countryName"}</div>
           </div>
         </div>
       ))}
